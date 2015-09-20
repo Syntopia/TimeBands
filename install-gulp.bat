47abe8ba40d702ec583b10b3913534001c5536e4
Mikael Christensen <mikaelhc@gmail.com>
2015-09-20 22:02:08 +0200

    Updates

---
+++ install-gulp.bat
@@ -0,0 +1,6 @@
+npm install gulp -g
+npm install gulp --save-dev
+npm install gulp-concat --save-dev
+npm install gulp-rename --save-dev
+npm install gulp-uglify --save-dev
+npm install gulp-sourcemaps --save-dev
---
+++ source/TextBoxStyle.js
@@ -0,0 +1,12 @@
+var TextBoxStyle = function() {
+    this.rx = 0;
+    this.ry = 0;
+    this.strokeWidth = 0.2;
+    this.textStroke = "#000";
+    this.textStrokeWidth = 0;
+    this.opacity = 1;
+    this.bgfill = "white";
+    this.textFill = "#000";
+    this.fontSize = 18;
+    this.section = false;
+};
